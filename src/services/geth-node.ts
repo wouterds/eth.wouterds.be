d581e03c0002ba7cff4d49e57495fdb1d29ad243
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-09-25 18:36:49 +0200

    feat: allow fetching transactions

--- src/services/geth-node.ts
+++ src/services/geth-node.ts
@@ -5,11 +5,30 @@ const GET_NODE = process.env.GETH_NODE || 'http://geth:8545';
 const provider = new ethers.JsonRpcProvider(GET_NODE);
 
 export const getLatestBlock = async () => {
-  return provider.getBlock('latest');
+  return getBlock('latest', { withTransactions: true });
 };
 
-export const getBlockByNumber = async (blockNumber: number) => {
-  return provider.getBlock(blockNumber);
+export const getBlock = async (
+  blockNumber: number | 'latest',
+  options?: { withTransactions: boolean },
+) => {
+  const block = await provider.getBlock(blockNumber, true);
+  if (!block) {
+    throw new Error('Block not found');
+  }
+
+  if (options?.withTransactions) {
+    const transactions = await Promise.all(
+      block.transactions.map(async (tx) => provider.getTransactionReceipt(tx)),
+    );
+
+    return {
+      ...block,
+      transactions,
+    };
+  }
+
+  return block;
 };
 
 export const getNetwork = async () => {
