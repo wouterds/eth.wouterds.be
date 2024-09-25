afe2f90f85d66380818bd48254d217780b0e57ea
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-09-25 18:36:30 +0200

    fix: hex decode

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -1,10 +1,11 @@
 export function hexToAscii(hex: string): string {
+  hex = hex.slice(2);
+
   let str = '';
   for (let i = 0; i < hex.length; i += 2) {
     const charCode = parseInt(hex.slice(i, i + 2), 16);
     str += charCode >= 32 && charCode <= 126 ? String.fromCharCode(charCode) : '.';
   }
 
-  // Check if the entire string is just dots (non-printable characters)
-  return str.replace(/^\.*$/, '');
+  return str.replace(/^\.*$/, '')?.trim();
 }
